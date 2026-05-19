665e3dd414390c023eeec5d05548525a7c4b6b41
zhenmeng sun <2159997489@qq.com>
2026-05-20 00:30:28 +0800

    fix: keep Go 1.26.2, restore govulncheck allowlist for known stdlib vulns

--- scripts/govulncheck.ps1
+++ scripts/govulncheck.ps1
@@ -1,6 +1,11 @@
 param(
   [string]$ServerPath = "",
-  [string[]]$Allowlist = @()
+  [string[]]$Allowlist = @(
+    "GO-2026-4918",
+    "GO-2026-4971",
+    "GO-2026-4977",
+    "GO-2026-4986"
+  )
 )
 
 $ErrorActionPreference = "Stop"
